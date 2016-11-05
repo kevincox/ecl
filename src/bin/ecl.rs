d5433a0363c1531b3596e29981b131fe94d7d6f5
Kevin Cox <kevincox@kevincox.ca>
2016-11-05 11:10:08 +0000

    Remove not working afl code.

--- src/bin/ecl.rs
+++ src/bin/ecl.rs
@@ -11,16 +11,12 @@ use erased_serde::Serialize;
 fn main() {
 	let args: Vec<String> = env::args().collect();
 	for path in &args[1..] {
-		println!("Path {}", path);
 		let mut ecl = fs::File::open(path).unwrap();
 		
 		let mut buf = String::new();
 		ecl.read_to_string(&mut buf).unwrap();
 		
 		let val = ecl::parse(&buf).unwrap();
-		println!("Parsing complete! {:?}", val);
 		val.erased_serialize(&mut ecl::lines::Serializer::new(std::io::stdout())).unwrap();
-		
-		println!("{}", buf);
 	}
 }
