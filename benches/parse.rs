45949ba9895b87b477e4df0c323a9f9530c9ec9a
Kevin Cox <kevincox@kevincox.ca>
2017-07-09 15:13:16 +0100

    Add some parser benchmarks.

---
+++ benches/parse.rs
@@ -0,0 +1,83 @@
+#![feature(test)]
+
+extern crate ecl;
+extern crate test;
+
+#[bench]
+fn parse_list(b: &mut test::Bencher) {
+	b.iter(||
+		ecl::parse(r###"
+			[
+				1
+				1 + 2
+				1 + 2 + 3
+				4
+				5
+				6
+				7
+				8
+				9
+				"str"
+				true
+				false
+			]
+		"###).unwrap()
+	)
+}
+
+#[bench]
+fn parse_dict(b: &mut test::Bencher) {
+	b.iter(||
+		ecl::parse(r###"
+			{
+				a = 1
+				b = 2
+				c = 3
+				d = 4
+				e = 5
+				f = 6
+				g = 7
+				nested = {
+					a = 1
+					b = 2
+					c = 3
+					d = 4
+				}
+			}
+		"###).unwrap()
+	)
+}
+
+#[bench]
+fn parse_dict_implicit(b: &mut test::Bencher) {
+	b.iter(||
+		ecl::parse(r###"
+			a = 1
+			b = 2
+			c = 3
+			d = 4
+			e = 5
+			f = 6
+			g = 7
+			nested = {
+				a = 1
+				b = 2
+				c = 3
+				d = 4
+			}
+		"###).unwrap()
+	)
+}
+
+#[bench]
+fn parse_dict_computed(b: &mut test::Bencher) {
+	b.iter(||
+		ecl::parse(r###"
+			{
+				"abc" = "val"
+				"a-${abc}-c" = 5
+				"${a-val-c}-${abc}" = false
+			}
+		"###).unwrap()
+	)
+}
