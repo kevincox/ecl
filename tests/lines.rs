1ed9c7d8889cccea21aeb87b32461c3725550d21
Kevin Cox <kevincox@kevincox.ca>
2018-03-05 21:20:05 +0000

    Fix lines test.

--- tests/lines.rs
+++ tests/lines.rs
@@ -6,13 +6,12 @@ mod utils;
 
 use erased_serde::Serialize;
 
-#[test]
-fn test_lines() {
-	for path in utils::scan_dir("lines", "ecl") {
+fn main() {
+	utils::test_dir("lines", "ecl", |path| {
 		let mut output = Vec::new();
 		ecl::eval_file(&path.to_string_lossy())
 			.erased_serialize(&mut ecl::lines::Serializer::new(&mut output)).unwrap();
 		
 		utils::diff(&output, &path.with_extension("lines"));
-	}
+	})
 }
